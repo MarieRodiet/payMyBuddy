0d8e540ed2af138ce69e8dc5ab9f8cd83fa32225
Marie Moore <68657110+MarieRodiet@users.noreply.github.com>
2023-09-01 15:26:02 +0200

    Fix date format in transfers Add logs

--- src/main/java/com/oc/paymybuddy/controller/TransfersController.java
+++ src/main/java/com/oc/paymybuddy/controller/TransfersController.java
@@ -7,21 +7,25 @@ import com.oc.paymybuddy.service.RecipientListService;
 import com.oc.paymybuddy.service.TransactionService;
 import com.oc.paymybuddy.service.UserAccountService;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import static org.apache.logging.log4j.LogManager.getLogger;
 
 import java.util.Date;
 import java.util.List;
 
 @Controller
 public class TransfersController {
+    private static final Logger logger = getLogger(TransfersController.class);
 
     @Autowired
     private UserAccountService userAccountService;
@@ -52,6 +56,7 @@ public class TransfersController {
                                @RequestParam(name="page", defaultValue = "0") Integer page,
                                @RequestParam(name="size", defaultValue = "10") Integer size){
         UserAccount currentUser = userAccountService.findCurrentUser();
+        logger.info("getting all transactions and connections associated to user");
         Page<Transaction> transactions  = transactionService.getTransactionsBySender(currentUser, PageRequest.of(page, size));
         List<UserAccount> connections = recipientListService.getRecipientListBySender(currentUser, userAccountService.findAllUserAccounts());
 
@@ -66,20 +71,25 @@ public class TransfersController {
 
     @PostMapping(path="/transfers")
     public String postTransaction(Model model,
-                               @Valid Transaction transaction,
+                               @Valid Transaction transaction, BindingResult result, RedirectAttributes redirectAttributes,
                                @RequestParam(name="page", defaultValue = "0") Integer page,
                                @RequestParam(name="size", defaultValue = "10") Integer size){
         UserAccount currentUser = userAccountService.findCurrentUser();
+        if(result.hasErrors()){
+            logger.error("could not add Transaction for user");
+        }
+        else{
+            Transaction newTransaction = new Transaction();
+            newTransaction.setSender(currentUser);
+            newTransaction.setRecipient(transaction.getRecipient());
+            newTransaction.setAmount(transaction.getAmount());
+            newTransaction.setDate(new Date());
+            newTransaction.setDescription(transaction.getDescription());
+            transactionService.saveTransaction(newTransaction);
 
-        Transaction newTransaction = new Transaction();
-        newTransaction.setSender(currentUser);
-        newTransaction.setRecipient(transaction.getRecipient());
-        newTransaction.setAmount(transaction.getAmount());
-        newTransaction.setDate(new Date());
-        newTransaction.setDescription(transaction.getDescription());
-        transactionService.saveTransaction(newTransaction);
-
-        userAccountService.decreaseUserAccountBalance(currentUser, transaction.getAmount());
+            userAccountService.decreaseUserAccountBalance(currentUser, transaction.getAmount());
+            logger.info("saving transaction and updating user's balance");
+        }
 
         Page<Transaction> transactions  = transactionService.getTransactionsBySender(currentUser, PageRequest.of(page, size));
         List<UserAccount> connections = recipientListService.getRecipientListBySender(currentUser, userAccountService.findAllUserAccounts());
@@ -103,10 +113,17 @@ public class TransfersController {
             RecipientList newRecipientList = new RecipientList();
             newRecipientList.setSender(currentUser);
             newRecipientList.setRecipient(userAccountService.findUserAccountByEmail(email));
-            recipientListService.saveRecipientList(newRecipientList);
-            redirectAttributes.addFlashAttribute("success", "This recipient was added successfully");
+            if(recipientListService.checkIfRecipientListExists(newRecipientList)){
+                redirectAttributes.addFlashAttribute("info", "This email is already in a recipient");
+            }
+            else{
+                logger.info("added new connection for current user");
+                recipientListService.saveRecipientList(newRecipientList);
+                redirectAttributes.addFlashAttribute("success", "This recipient was added successfully");
+            }
         }
         else{
+            logger.error("could not add new connection");
             redirectAttributes.addFlashAttribute("error", "No user known with this email address");
         }
         return "redirect:/transfers";
