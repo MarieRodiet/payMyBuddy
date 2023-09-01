17ece22fd74b5bb7c6b80aa1b5b5cd7c971add78
Marie Moore <68657110+MarieRodiet@users.noreply.github.com>
2023-09-01 16:34:16 +0200

    Added timestamp to Transactions Added sorting of transactions descending by date

--- src/main/java/com/oc/paymybuddy/service/TransactionService.java
+++ src/main/java/com/oc/paymybuddy/service/TransactionService.java
@@ -6,6 +6,7 @@ import com.oc.paymybuddy.repository.TransactionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -18,7 +19,14 @@ public class TransactionService {
     private TransactionRepository transactionRepository;
 
     public Page<Transaction> getTransactionsBySender(UserAccount userAccount, PageRequest pageRequest){
-        Page<Transaction> transactions = transactionRepository.findTransactionsBySender(userAccount, pageRequest);
+        Sort sortByDate = Sort.by(Sort.Order.desc("date"));
+        PageRequest sortedPageRequest = PageRequest.of(
+                pageRequest.getPageNumber(),
+                pageRequest.getPageSize(),
+                sortByDate
+        );
+
+        Page<Transaction> transactions = transactionRepository.findTransactionsBySender(userAccount, sortedPageRequest);
         return transactions;
     }
 
